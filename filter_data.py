f0ebc7b1f1486f29b655f01e3444d5a05938ef2a
Prince Patel <princempatel2007@gmail.com>
2026-06-29 16:23:53 +1000

    brought pd.concat out of the loop to increase speed efficiency

--- filter_data.py
+++ filter_data.py
@@ -4,16 +4,16 @@ INPUT_FILES = ['data/daily_sales_data_0.csv', 'data/daily_sales_data_1.csv', 'da
 OUTPUT_FILE = 'data/filtered_sales_data.csv'
 
 def filter_data(input_files, output_file):
-    filtered_data = pd.DataFrame()
+
+    data = []
 
     for file in input_files:
         df = pd.read_csv(file)
-        filtered_df = df[df['product'] == 'pink morsel']
-        filtered_df = filtered_df.copy()
-        filtered_df['sales'] = filtered_df['price'].str.replace('$', '', regex=False).astype(float) * filtered_df['quantity']
-        filtered_data = pd.concat([filtered_data, filtered_df], ignore_index=True)
-        filtered_data = filtered_data[['sales', 'date', 'region']].sort_values('region', ascending=True)
+        filtered_df = df[df['product'] == 'pink morsel'].copy()
+        filtered_df.loc[:, 'sales'] = filtered_df['price'].str.replace('$', '', regex=False).astype(float) * filtered_df['quantity']
+        data.append(filtered_df[['sales', 'date', 'region']])
 
+    filtered_data = pd.concat(data, ignore_index=True).sort_values(by='region', ascending=True)
     filtered_data.to_csv(output_file, index=False)
 
 if __name__ == '__main__':
