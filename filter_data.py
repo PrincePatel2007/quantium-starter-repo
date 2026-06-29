ff9513049acf44c3aa620d2157b1962a296cd597
Prince Patel <princempatel2007@gmail.com>
2026-06-29 15:30:59 +1000

    automating combining data process of 3 files

---
+++ filter_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+INPUT_FILES = ['data/daily_sales_data_0.csv', 'data/daily_sales_data_1.csv', 'data/daily_sales_data_2.csv']
+OUTPUT_FILE = 'data/filtered_sales_data.csv'
+
+def filter_data(input_files, output_file):
+    filtered_data = pd.DataFrame()
+
+if __name__ == '__main__':
+    filter_data(INPUT_FILES, OUTPUT_FILE)
